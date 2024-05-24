0443afebd4c2e3b824ab2c1a2e7a0e021f093e1d
AC <2312932681@qq.com>
2024-05-24 15:57:49 +0800

    提交修改增加被忽略的文件

---
+++ test/unit.webapp/unit.webapp.common/Filters/ActionLogFilter.cs
@@ -0,0 +1,66 @@
+﻿
+/*
+ * Copyright (c) 2024 星曳数据
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * This file is provided under the Mozilla Public License Version 2.0,
+ * and the "NO WARRANTY" clause of the MPL is hereby expressly
+ * acknowledged.
+ */
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+using System.Diagnostics;
+
+namespace unit.webapp.common.Filters
+{
+    public  class ActionLogFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            ActionExecutedContext actionContext = await next();
+            sw.Stop();
+
+            HttpRequest request = context.HttpContext.Request;
+
+            string requstBody = string.Empty;
+
+            string url = (request.IsHttps ? "https" : "http") + "://" + request.Host + request.Path + request.QueryString.ToString();
+            if (request.Method == HttpMethods.Post || request.Method == HttpMethods.Put)
+            {
+
+                if (request.HasFormContentType)
+                {
+                    IFormCollection form = await request.ReadFormAsync();
+                    Dictionary<string, string> filteredForm = new();
+                    foreach (KeyValuePair<string, Microsoft.Extensions.Primitives.StringValues> field in form)
+                    {
+                        filteredForm[field.Key] = field.Value.ToString();
+                    }
+
+                    requstBody = string.Join("&", filteredForm.Select(kv => $"{kv.Key}={kv.Value}"));
+                }
+                else
+                {
+                    request.Body.Seek(0, SeekOrigin.Begin);
+                    // 读取body的数据流，并转为string
+                    using (var sr = new StreamReader(request.Body))
+                    {
+                        requstBody = await sr.ReadToEndAsync();
+                    }
+                    request.Body.Seek(0, SeekOrigin.Begin);
+                }
+            }
+            else
+            {
+                requstBody = request.QueryString.ToString();
+            }
+            string s = "123132";
+        }
+    }
+}
